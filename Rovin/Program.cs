019441b6e2bb8a9ae341d4d8eb93a5185c56aa24
Muthu Dharmalingam <muthudharmalingam@muthus-mbp.myfiosgateway.com>
2022-05-20 12:40:36 -0400

    push

--- Rovin/Program.cs
+++ Rovin/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => { options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod()); });
+builder.Host.UseSerilog((ctx,lc)=>lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
